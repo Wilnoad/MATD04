8d58c0f3ca39ac2a0d6aa5986d8083a1c916d415
Wilson Santos <42124099+Wilnoad@users.noreply.github.com>
2019-10-06 19:50:19 -0300

    Criado o arquivo base.
    Será elaborado o exercício.

---
+++ atividades/listas/IntegerList.h
@@ -0,0 +1,13 @@
+A. Implementar funções em C para manipular listas lineares de elementos inteiros; definir um novo tipo (com typedef): IntegerList. A assinatura de cada função deve ser igual a que está especificada a seguir.
+
+int isEmpty(IntegerList *) /* returns 0 if the argument is a non-empty list, 1 otherwise; */
+
+int size(IntegerList *) /* returns the size of the list; */
+
+void insertElement(IntegerList **, int) /* insert node with integer element at the end of the list; */
+
+void printList(IntegerList *) /* prints the integer value of each of the list elements. */
+
+B. Colocar o código no GitHub, em um repositório seu, individual, chamado de "matd04", na subpasta (folder) "atividades/listas" (estrutura matd04/atividades/listas/) . Usar um arquivo .h para definir tipos e protótipos de funções usadas (ver https://github.com/christinaflach/aulas/blob/master/matd04/atividades/listas/IntegerList.h).
+
+Obs.: Não é preciso disponibilizar a função "main".
